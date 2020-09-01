e5c9081c60d7ab27d15813ccc621dc281cff266e
Somanath Krishnaswamy <soma17@uw.edu>
2020-08-31 22:23:09 -0700

    Added a server route to get token and changed the client App from function to class

--- givecalendar2020/server.js
+++ givecalendar2020/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,15 +9,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
+// Add a route to get the token
+app.get('/api/getToken', async (req, res) => {
+    const stringToEncode = process.env.CLIENT_ID+':'+process.env.CLIENT_SECRET;
+    const buff = Buffer.alloc((process.env.CLIENT_ID.length + process.env.CLIENT_SECRET.length +1), stringToEncode);
+    const base64EncodedString = buff.toString('base64');
+    const headerValue = 'Basic ' + base64EncodedString
 
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
+    var requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/x-www-form-urlencoded',
+            'Authorization' : headerValue
+        },
+        redirect: 'follow'
+      };
+
+    const response = await fetch("https://public-api.benevity-staging.org/oauth2/token?grant_type=client_credentials&scope=benevity/api", requestOptions)
+    .then(response => response.json())
+    .then(result => res.json(result))
+    .catch(error => console.log('error', error));
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
